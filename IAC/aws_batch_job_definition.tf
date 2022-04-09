7e8a997a6d1fd646c4bcdcbf7b6fffe7697dbf0e
Airton Finoti <afinoti@users.noreply.github.com>
2022-04-09 00:20:22 -0300

    mudando imagem padrão

--- IAC/aws_batch_job_definition.tf
+++ IAC/aws_batch_job_definition.tf
@@ -17,7 +17,10 @@ resource "aws_batch_job_definition" "batch_example_jobdef" {
     {"type": "MEMORY", "value": "${var.job_definition_resource_memory}"}
   ],
   "executionRoleArn": "${aws_iam_role.batch-example-execution-role.arn}",
-  "jobRoleArn": "${aws_iam_role.batch-example-job-role.arn}"
+  "jobRoleArn": "${aws_iam_role.batch-example-job-role.arn}",
+  "networkConfiguration": { 
+    "assignPublicIp": "ENABLED"
+  },
 }
 CONTAINER_PROPERTIES
   tags                 = var.default_tags
