d4525baf856e1aad7dff42416e04fafff5c99190
Airton Finoti <afinoti@users.noreply.github.com>
2022-04-10 10:33:48 -0300

    snap

--- IAC/variables.tf
+++ IAC/variables.tf
@@ -40,7 +40,7 @@ variable "compute_environment_name" {
 
 variable "job_definition_image_full_url" {
   type        = string
-  default     = "016431746308.dkr.ecr.us-east-1.amazonaws.com/example_python_flask:v1.0.2"
+  default     = "016431746308.dkr.ecr.us-east-1.amazonaws.com/example_python_flask:v1.0.3"
   description = "Job Definition Image Full URL"
 }
 
