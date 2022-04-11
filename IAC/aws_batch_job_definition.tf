dcb7113475c4119404e775e109296226c8fdc7d4
Airton Finoti <afinoti@users.noreply.github.com>
2022-04-10 23:17:29 -0300

    log

--- IAC/aws_batch_job_definition.tf
+++ IAC/aws_batch_job_definition.tf
@@ -1,7 +1,7 @@
-resource "aws_cloudwatch_log_group" "example" {
-  name              = "batch_example_logs"
-  retention_in_days = 14
-}
+# resource "aws_cloudwatch_log_group" "example" {
+#   name              = "batch_example_logs"
+#   retention_in_days = 14
+# }
 
 
 resource "aws_batch_job_definition" "batch_example_jobdef" {
@@ -13,7 +13,7 @@ resource "aws_batch_job_definition" "batch_example_jobdef" {
 
   container_properties = <<CONTAINER_PROPERTIES
 {
-  "command"     : [],
+  "command"     : ["python","entry_shell.py","inverso","123"],
   "image"       : "${var.job_definition_image_full_url}",
   "fargatePlatformConfiguration": {
     "platformVersion": "LATEST"
