d1fa543fafdfcb302f2755dfd562062f95fa4530
Airton Finoti <afinoti@users.noreply.github.com>
2022-04-12 13:46:02 -0300

    new version

--- send_job.py
+++ send_job.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
 "Submit a job to AWS Batch."
-
+from pprint import pprint
 import boto3
 
 batch = boto3.client("batch")
 
-response = batch.submit_job(
-    jobName="jdoe-test-job",  # use your HutchNet ID instead of 'jdoe'
-    jobQueue="example_batch_queue",  # sufficient for most jobs
-    jobDefinition="batch_example_jobdefinition:27",  # use a real job definition
-    containerOverrides={
-        "command": [
-            "python",
-            "entry_shell.py",
-            "echo",
-            "ECHHOO Hello, World!",
-        ],  # optionally override command
-        "environment": [  # optionally set environment variables
-            {"name": "FAVORITE_COLOR", "value": "blue"},
-            {"name": "FAVORITE_MONTH", "value": "December"},
-        ],
-    },
+result = batch.describe_job_definitions(
+    jobDefinitionName="batch_example_jobdefinition", status="ACTIVE"
 )
 
-print("Job ID is {}.".format(response["jobId"]))
+job_def_name = None
+latest = 0  # latest version of the job definition
+for job_def in result["jobDefinitions"]:
+    if job_def["revision"] > latest:
+        latest = job_def["revision"]
+        job_def_name = job_def["jobDefinitionName"] + ":" + str(latest)
+        # pprint(job_def_name)
+
+if job_def_name is None:
+    raise ValueError("No job definition found")
+else:
+    print(f"Found job definition {job_def_name}")
+    print("Submitting job...")
+    response = batch.submit_job(
+        jobName="jdoe-test-job",  # use your HutchNet ID instead of 'jdoe'
+        jobQueue="example_batch_queue",  # sufficient for most jobs
+        jobDefinition="batch_example_jobdefinition:28",  # use a real job definition
+        containerOverrides={
+            "command": [
+                "python",
+                "entry_shell.py",
+                "echo",
+                "ECHHOO Hello, World!",
+            ],  # optionally override command
+            "environment": [  # optionally set environment variables
+                {"name": "FAVORITE_COLOR", "value": "blue"},
+                {"name": "FAVORITE_MONTH", "value": "December"},
+            ],
+        },
+    )
+
+    print("Job ID is {}.".format(response["jobId"]))
